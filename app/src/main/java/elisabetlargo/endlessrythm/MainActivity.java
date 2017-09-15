c1226065ea865857a0d8312872248b2595f766fe
Elisabet <elilargo1994@hotmail.com>
2017-09-15 17:04:47 +0200

    Branch develop

---
+++ app/src/main/java/elisabetlargo/endlessrythm/MainActivity.java
@@ -0,0 +1,13 @@
+package elisabetlargo.endlessrythm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//`prueba
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
